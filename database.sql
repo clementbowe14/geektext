2b81a52703f3fcbf994896ceb443e467e5894024
Vexiom3 <99135265+Vexiom3@users.noreply.github.com>
2022-02-22 07:15:05 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@ CREATE TABLE "User" (
     username VARCHAR(30),
     password VARCHAR(30),
     first_name VARCHAR(30),
-    last_name VARCHAR(30)
+    last_name VARCHAR(30),
 );
 
 CREATE TABLE CreditCard (
@@ -11,4 +11,39 @@ CREATE TABLE CreditCard (
     card_number VARCHAR(16),
     expiration_date VARCHAR(5),
 	card_user_id Serial REFERENCES "User"(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE Book (
+    ISBN INT PRIMARY KEY,
+    authorID INT REFERENCES Author (authorKey),
+    firstName VARCHAR,
+    lastName VARCHAR,
+    publisher VARCHAR,
+    biography VARCHAR,
+);
+
+CREATE TABLE Author (
+    authorKey INT PRIMARY KEY,
+    bookName VARCHAR,
+    bookDescription VARCHAR,
+    bookPrice INT,
+    bookGenre VARCHAR,
+    bookPublisher VARCHAR,
+    yearPublished INT
+    copiesSold INT,
+);
+
+CREATE TABLE Wishlist (
+    wishlist_id SERIAL PRIMARY KEY,
+    user_id Serial REFERENCES "User"(user_id) NOT NULL,
+    ISBN INT[] REFERENCES Book(ISBN) NOT NULL
+);
+
+CREATE TABLE ReviewedBooks(
+    review_id integer SERIAL PRIMARY KEY,
+    rating_score integer,
+    date_stamp date,
+    review_text VARCHAR,
+ 	review_user_id Serial REFERENCES "User"(user_id) ON DELETE CASCADE,
+	review_ISBN Serial REFERENCES "Book"(ISBN) ON DELETE CASCADE
 );
